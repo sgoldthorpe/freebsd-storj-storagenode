5632272dce1e7fc1c1c2d67f18d553e369097839
Steve Goldthorpe <4780040-sgoldthorpe@users.noreply.gitlab.com>
2021-06-05 14:24:10 +0000

    complete rework and polish.
    We now don't really care about the $HOME/bin/storagenode file, just the
    -latest file.

--- update-storagenode.sh
+++ update-storagenode.sh
@@ -3,55 +3,78 @@
 # GLOBALS
 VERBOSE=0
 SN_BIN=storagenode
-SN_BIN_LATEST=${SN_BIN}-latest
-SCNAME="`basename $0`"
-TMPDIR="`mktemp -d \"/tmp/${SCNAME}.XXXXXX\"`"
-
-if [ $? -ne 0 ]; then
-	echo "$0: Can't create temp directory, exiting..." >&2
-	exit 1
-fi
+SN_BIN_LATEST="${SN_BIN}-latest"
+VERSION_URL=https://version.storj.io
+LATEST_BASEURL="https://github.com/storj/storj/releases/latest/download"
 
 # FUNCS
 
+err() {
+	EXNO=$1
+	shift
+	echo "${SCNAME}: $*" >&2
+	rm -f "$TMPDIR"/*; rmdir "$TMPDIR"
+	exit $EXNO
+}
+
+msg() {
+	test $VERBOSE -gt 0 && echo "$*"
+}
+
 fetch_zip() {
-	URL=$1
-	DEST=$2
-	ZFILE=$TMPDIR/sn.zip
-	test $VERBOSE -gt 0 && echo "downloading ${URL}..."
-	curl -L -s -o $ZFILE $URL >/dev/null 2>&1
+	URL="$1"
+	DEST="$2"
+	ZIP="$TMPDIR/sn.zip"
+	msg "downloading ${URL}..."
+	curl -L -s -o "$ZIP" "$URL" >/dev/null 2>&1
 	if [ $? -ne 0 ]; then
-		rm -f $TMPDIR/*; rmdir $TMPDIR
-		echo "$0: Problem downloading, exiting..." >&2
-		exit 1
+		err 1 "Problem downloading, exiting..."
 	fi
-	unzip -o $ZFILE -d $TMPDIR >/dev/null 2>&1
-	if [ $? -ne 0 -o ! -f $TMPDIR/$SN_BIN ]; then
-		rm -f $TMPDIR/*; rmdir $TMPDIR
-		echo "$0: Problem extracting, exiting..." >&2
-		exit 1
+	unzip -o "$ZIP" -d "$TMPDIR" >/dev/null 2>&1
+	if [ $? -ne 0 -o ! -f "$TMPDIR/$SN_BIN" ]; then
+		err 1 "Problem extracting, exiting..."
 	fi
 
-	cp -p $TMPDIR/$SN_BIN $DEST
-	chmod 755 $DEST # ensure perms are OK
+	cp -p "$TMPDIR/$SN_BIN" "$DEST"
+	chmod 755 "$DEST" # ensure perms are OK
 }
 
 get_ver() {
-	echo "`$1 version 2>/dev/null| awk '/^Version/ { sub(/^v/,"",$2);print $2 }'`"
+	echo "`\"$1\" version 2>/dev/null| awk '/^Version/ { sub(/^v/,"",$2);print $2 }'`"
 }
 
 get_sugg_ver() {
-	jq -r .processes.storagenode.suggested.version $1 2>/dev/null
+	jq -r .processes.storagenode.suggested.version "$1" 2>/dev/null
 }
 
 get_sugg_url() {
-	jq -r .processes.storagenode.suggested.url $1 | sed -e "s/{os}/$OS/" -e "s/{arch}/$ARCH/" -e 's/\.exe//'
+	jq -r .processes.storagenode.suggested.url "$1" | sed -e "s/{os}/$OS/" -e "s/{arch}/$ARCH/" -e 's/\.exe//'
 }
 
 fetch_storj_vers() {
-	test $VERBOSE -gt 0 && echo "Fetching version information..."
-	curl -L -s -o $1 https://version.storj.io >/dev/null 2>&1
+	msg "Fetching version information..."
+	curl -L -s -o "$1" "$VERSION_URL" >/dev/null 2>&1
 }
+
+fetch_and_version() {
+	fetch_zip "$1" "$TMPDIR/$SN_BIN_LATEST"
+	VER="`get_ver \"$TMPDIR/$SN_BIN_LATEST\"`"
+	if [ -z "$VER" ]; then
+		err 1 "Couldn't get version from downloaded binary"
+	fi
+	mv "$TMPDIR/$SN_BIN_LATEST" "$HOME/bin/${SN_BIN}-${VER}"
+	# create link then use mv to change link atomically
+	ln -s "${SN_BIN}-${VER}" "$HOME/bin/${SN_BIN_LATEST}.new"
+	mv "$HOME/bin/${SN_BIN_LATEST}.new" "$HOME/bin/${SN_BIN_LATEST}"
+}
+
+# create temporary working directory
+SCNAME="`basename \"$0\"`"
+TMPDIR="`mktemp -d \"/tmp/${SCNAME}.XXXXXX\"`"
+
+if [ $? -ne 0 ]; then
+	err 1 "Can't create temp directory, exiting..."
+fi
 
 # CHECK ARGS
 
@@ -59,7 +82,7 @@ if [ "x$1" = "x-v" ]; then
 	VERBOSE=1
 fi
 
-# Storj only support amd64 on freebsd so hardcode
+# Storj only support amd64 on FreeBSD so hard-code for now (ditto linux)
 ARCH=amd64
 # Assume anything not FreeBSD is Linux (for testing)
 case `uname -o` in
@@ -69,63 +92,48 @@ case `uname -o` in
 		OS=linux;;
 esac
 
-LATEST_ZIP=https://github.com/storj/storj/releases/latest/download/${SN_BIN}_${OS}_${ARCH}.zip
+# create users bin directory if it doesn't exist
+test -d "$HOME/bin" || mkdir "$HOME/bin"
 
-test -d $HOME/bin || mkdir $HOME/bin
-
-# if the storagenode binary doesn't exist, fetch it from github
-if [ ! -f $HOME/bin/$SN_BIN ]; then
-	fetch_zip ${LATEST_ZIP} $HOME/bin/$SN_BIN
+# if the storagenode latest binary doesn't exist, fetch it from github
+if [ ! -f "$HOME/bin/$SN_BIN_LATEST" ]; then
+	msg "$SN_BIN_LATEST missing, fetching from github..."
+	fetch_and_version "$LATEST_BASEURL/${SN_BIN}_${OS}_${ARCH}.zip"
 fi
 
-# if the storagenode-latest file doesn't exist, just copy the storagenode binary
-if [ ! -f $HOME/bin/$SN_BIN_LATEST ]; then
-	cp -p $HOME/bin/$SN_BIN $HOME/bin/$SN_BIN_LATEST
+# if the storagenode binary doesn't exist, just copy it from latest
+if [ ! -f "$HOME/bin/$SN_BIN" ]; then
+	msg "$SN_BIN missing, copying from $SN_BIN_LATEST..."
+	cp -p "$HOME/bin/$SN_BIN_LATEST" "$HOME/bin/$SN_BIN"
 fi
 
-V_FILE=$TMPDIR/versions.json
-fetch_storj_vers $V_FILE
+V_FILE="$TMPDIR/versions.json"
+fetch_storj_vers "$V_FILE"
 
 if [ $? -ne 0 ]; then
-	rm -f $TMPDIR/*; rmdir $TMPDIR
-	echo "$0: Can't fetch remote versions, exiting..." >&1
-	exit 1
+	err 1 "Can't fetch remote versions, exiting..."
 fi
 
-S_VER="`get_sugg_ver $V_FILE`"
-L_VER="`get_ver $HOME/bin/$SN_BIN`"
+S_VER="`get_sugg_ver \"$V_FILE\"`"
+C_VER="`get_ver \"$HOME/bin/$SN_BIN_LATEST\"`"
 
-if [ "x$L_VER" = "x" -o "x$L_VER" = "x" ]; then
-	rm -f $TMPDIR/*; rmdir $TMPDIR
-	echo "$0: Problem detecting versions, exiting..." >&1
-	exit 1
+if [ "x$C_VER" = "x" -o "x$S_VER" = "x" ]; then
+	err 1 "Problem detecting versions, exiting..."
 fi
 
-if [ $VERBOSE -gt 0 ]; then
-	echo "Local version: $L_VER"
-	echo "Suggested version: $S_VER"
+msg "Current version: $C_VER"
+msg "Suggested version: $S_VER"
+
+if [ "$S_VER" != "$C_VER" ]; then
+	msg "Update detected - upgrade from version $C_VER to $S_VER"
+	URL="`get_sugg_url \"$V_FILE\"`"
+	fetch_and_version "$URL"
+
+	msg "Created $HOME/bin/$SN_BIN_LATEST"
+	msg "service will need to be stopped, $SN_BIN_LATEST copied to $SN_BIN and service restarted."
+else
+	msg "Nothing to do."
 fi
 
-if [ "$S_VER" != "$L_VER" ]; then
-	LATEST_VER="`get_ver $HOME/bin/$SN_BIN_LATEST`"
-	if [ "$S_VER" = "$LATEST_VER" ]; then
-		rm -f $TMPDIR/*; rmdir $TMPDIR
-		date
-		echo "Already downloaded suggested version as $SN_BIN_LATEST but it is not installed as $SN_BIN"
-		echo "root will need to stop storage node and rename command and restart storage node."
-		exit 0
-	fi
-	URL="`get_sugg_url $V_FILE`"
-	echo "Update detected - upgrade from version $L_VER to $S_VER"
-	cp -p $HOME/bin/$SN_BIN $HOME/bin/${SN_BIN}-${L_VER}
-	fetch_zip $URL $HOME/bin/$SN_BIN_LATEST
-
-	echo "Created $HOME/bin/$SN_BIN_LATEST"
-	echo "root will need to stop storage node and rename command and restart storage node."
-	echo "---"
-elif [ $VERBOSE -ne 0 ]; then
-	echo "Versions are equal - nothing to do."
-fi
-
-rm -f $TMPDIR/*; rmdir $TMPDIR
+rm -f "$TMPDIR"/*; rmdir "$TMPDIR"
 exit 0
